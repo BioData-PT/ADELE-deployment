3660d092f7cfdd67b93db874dffe9e67d02e07fe
Marta Félix <marta.felix@tecnico.ulisboa.pt>
2025-09-23 11:25:20 +0100

    fix: get_cert.sh and comment bo-api HTTPS connection

--- gdi-core/nginx/get_cert.sh
+++ gdi-core/nginx/get_cert.sh
@@ -6,6 +6,7 @@ docker compose run --rm certbot certonly \
     -d fdp.adele.inesc-id.pt -d rems.adele.inesc-id.pt \
     -d inbox.adele.inesc-id.pt -d login.adele.inesc-id.pt \
     -d download.adele.inesc-id.pt -d backoffice.adele.inesc-id.pt \
-    -d website.adele.inesc-id.pt -d website-api.adele.inesc-id.pt
+    -d website.adele.inesc-id.pt -d website-api.adele.inesc-id.pt \
+    -d backoffice-api.adele.inesc-id.pt
 
 
