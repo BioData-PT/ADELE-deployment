5ad096159a7ba1c5b64625e4a4596cc5236f7344
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2025-12-18 20:27:06 +0000

    fix: certbot now renews certs

--- gdi-core/nginx/get_cert.sh
+++ gdi-core/nginx/get_cert.sh
@@ -3,6 +3,7 @@
 docker compose run --rm certbot certonly \
     --webroot --webroot-path=/var/www/certbot \
     --agree-tos --no-eff-email \
+    --cert-name adele-certs \
     -d fdp.adele.inesc-id.pt -d rems.adele.inesc-id.pt \
     -d inbox.adele.inesc-id.pt -d login.adele.inesc-id.pt \
     -d download.adele.inesc-id.pt -d backoffice.adele.inesc-id.pt \
